31baec62206cde2fd1f4a276e2e9755187e6fa9d
Brandon Cen <bcen96@gmail.com>
2015-02-20 18:24:37 -0800

    test gun, shooting

---
+++ Assets/Scripts/TestScripts/Bullet_Behavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet_Behavior : MonoBehaviour {
+
+	private float force = 2000.0f;
+	// Use this for initialization
+	void Start () {
+		rigidbody.AddForce (transform.right * force);
+		Debug.Log ("Bullet Shot");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//gameObject.transform.position += (Time.deltaTime * speed * transform.forward);
+	}
+
+	void OnCollisionEnter(){
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/TestScripts/Gun_Fire.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun_Fire : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float spawn_distance = 3.0f;
+	
+		if (Input.GetMouseButtonDown (0)) {
+
+			GameObject.Instantiate ((GameObject)Resources.Load("TestBullet"), transform.position + spawn_distance * transform.right, transform.rotation);
+		}
+	}
+}
