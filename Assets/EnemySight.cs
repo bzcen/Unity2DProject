78b79d2ce22ff9d43f93d7849901f994b32ba54e
Derek Tanaka <tanaka.derek@gmail.com>
2015-02-22 13:46:33 -0600

    enemy sees out his ass

---
+++ Assets/EnemySight.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySight : MonoBehaviour {
+
+	public GameObject player;
+	private Ray sight;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void FixedUpdate () {
+
+		Vector3 directionFacing = new Vector3 (Mathf.Sin(transform.localEulerAngles.z*Mathf.PI/180f), -Mathf.Cos(transform.localEulerAngles.z*Mathf.PI/180f));
+		sight = new Ray(transform.position, directionFacing);
+		//Debug.Log (Mathf.Cos (transform.localEulerAngles.z * Mathf.PI / 180f) + " " + Mathf.Sin (transform.localEulerAngles.z * Mathf.PI / 180f));
+		RaycastHit hit;
+		if (Physics.Raycast (sight, out hit, 10f)) {
+			if(hit.collider.tag == "Player"){
+				Debug.Log("player found");
+			}
+		}
+	}
+}
--- Assets/Scripts/TestScripts/Gun_Fire.cs
+++ Assets/Scripts/TestScripts/Gun_Fire.cs
@@ -12,7 +12,7 @@ public class Gun_Fire : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		float spawn_distance = 3.0f;
+		float spawn_distance = 2.5f;
 	
 		if (Input.GetMouseButtonDown (0)) {
 
