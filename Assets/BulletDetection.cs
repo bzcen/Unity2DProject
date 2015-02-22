b64ab7199804d7cb70878c2ed4dd90cd41134292
Derek Tanaka <tanaka.derek@gmail.com>
2015-02-22 02:19:14 -0600

    added our first enemy boys

---
+++ Assets/BulletDetection.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletDetection : MonoBehaviour {
+
+	public void OnCollisionEnter(Collision other){
+		if (other.gameObject.name == "TestBullet(Clone)") {
+			Destroy (this.gameObject);
+		} else if (other.gameObject.name == "TestCharacter") {
+			Debug.Log("player is touching an enemy");
+		}
+	}
+
+}
