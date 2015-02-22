aec53e08e2347d6beed95885b7c62f3547c9fa2c
Derek Tanaka <tanaka.derek@gmail.com>
2015-02-22 13:54:19 -0600

    enemy kinda moves towards us on sight

--- Assets/EnemySight.cs
+++ Assets/EnemySight.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class EnemySight : MonoBehaviour {
 
 	public GameObject player;
+	public float speed = 20.0f;
 	private Ray sight;
+	private Rigidbody enemyRigidBody;
 
 	// Use this for initialization
 	void Start () {
+		enemyRigidBody = GetComponent <Rigidbody> ();
 	}
 
 	void FixedUpdate () {
@@ -19,6 +22,7 @@ public class EnemySight : MonoBehaviour {
 		if (Physics.Raycast (sight, out hit, 10f)) {
 			if(hit.collider.tag == "Player"){
 				Debug.Log("player found");
+				enemyRigidBody.AddForce(directionFacing);
 			}
 		}
 	}
