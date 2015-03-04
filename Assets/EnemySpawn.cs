66091a263b8145c9f7250b22e13125b375c55ebf
Jiang333 <kevi124324@gmail.com>
2015-03-04 08:47:51 -0500

    spawner

---
+++ Assets/EnemySpawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehaviour {
+	public float spawnTime = 5f;
+	public float spawnDelay = 3f;
+	public GameObject[] enemies;		
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("E_1", spawnDelay, spawnTime);
+	}
+
+	void E_1 () {
+		int enemyIndex = Random.Range(0, enemies.Length);
+		Instantiate(enemies[enemyIndex], transform.position, transform.rotation);
+
+		foreach(ParticleSystem p in GetComponentsInChildren<ParticleSystem>())
+		{
+			p.Play();
+		}
+	}
+}
