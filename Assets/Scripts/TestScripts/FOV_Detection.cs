4c7c269e4bb5b4b31b25b688dd48d7169659a7d7
Brandon Cen <bcen96@gmail.com>
2015-03-17 19:41:36 -0700

    better field of vision, accounts for obstructions

--- Assets/Scripts/TestScripts/FOV_Detection.cs
+++ Assets/Scripts/TestScripts/FOV_Detection.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class FOV_Detection : MonoBehaviour {
 
+
 	// Use this for initialization
 	void Start () {
 	
@@ -13,8 +14,25 @@ public class FOV_Detection : MonoBehaviour {
 	
 	}
 	void OnTriggerEnter2D(Collider2D other){
+		// if the player enters the collision cone field
 		if (other.gameObject.tag == "Player"){
-			Debug.Log("Player collided");
+
+			// set a raycast in the direction of the player from the beginning point of the cone
+			var rayDirection = other.transform.position - transform.position;
+			// fire the raycast in that direction from the beginning point position
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDirection);
+
+			// BIG NOTE: this only works if the object that holds the cone collision box is set to "Ignore Raycast" in "Layer", or else the raycast will immediately just hit the cone itself
+			if (hit.collider != null) {
+				if (hit.transform.tag == "Player") {
+			
+					Debug.Log ("Player collided");
+				} else {
+					Debug.Log ("Something is in the way");
+				}
+			} else {
+				Debug.Log ("Nothing hit!");
+			}
 		}
 
 	}
